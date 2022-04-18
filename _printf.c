59446eed084a5e8422fdd34fda49338eacafa131
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-18 17:57:09 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,62 +1,49 @@
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
 #include <stdarg.h>
-#include <stdlib.h>
-
 /**
- * _printf - print a formatted text
- * @format: str with the text and format
- * Return: the number of bytes that print
+ * _printf - produces output according to format
+ * @format: format string containing zero or more directives
+ *
+ * Return: the number of characters printed, excluding null byte
  */
-
 int _printf(const char *format, ...)
 {
-
-	int bytes = 0, bts = 0;
-	char *head, *current, *init, *buffer, *copy;
-	int (*execute)(va_list ap, char **buffer);
+	data_t types[] = {
+		{"c", _printchar}, {"s", _printstring}, {"i", _printint},
+		{"R", _rot13}, {"d", _printint}, {"r", _printrev}};
+	int i, j, counter = 0;
 	va_list ap;
 
-	if (!format)
-		return (-1);
-
 	va_start(ap, format);
-	current = head = init = (char *) format;
-
-	buffer = malloc(1024 * sizeof(char));
-	if (!buffer)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	copy = buffer;
-
-	while (*head)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (*head == '%' || *current == '%')
+		if (format[i] != '%')
+			counter += _putchar(format[i]);
+		else if (format[i + 1] == '%')
 		{
-			if (*current != '%')
-				current = head;
-
-			if (*(head + 1) == 32 || *(head + 1) == 9)
-			{
-				head++;
-				continue;
-			}
-			execute = get_ops_function(*(head + 1), &buffer, &bytes);
-			bts = execute(ap, &buffer);
-			current = init;
-			buffer += bts, bytes += bts;
-			head += 2;
+			counter += _putchar('%');
+			i++;
+		}
+		else if (format[i + 1] != 'c' && format[i + 1] != 's'
+			 && format[i + 1] != 'i' && format[i + 1] != 'd'
+			 && format[i + 1] != 'R' && format[i + 1] != 'r')
+		{
+			counter += _putchar('%');
+			counter += _putchar(format[i + 1]);
+			i++;
 		}
 		else
-			*buffer++ = *head++, bytes++;
+		{
+			for (j = 0; j < 6; j++)
+				if (format[i + 1] == *types[j].type)
+					counter += types[j].func(ap);
+			i++;
+		}
 	}
-
-	buffer = copy;
-	write(1, buffer, bytes);
-	free(buffer);
 	va_end(ap);
-	printf("%d\n", bytes);
-	return (bytes);
+	return (counter);
 }
 
 
