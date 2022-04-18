75698e61a85c159d6ec99b66fafdb2aeb3546772
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-18 18:14:27 +0300

    Update main.h

--- main.h
+++ main.h
@@ -2,38 +2,42 @@
 #define _MAIN_H_
 
 #include <stdarg.h>
-#include <stdlib.h>
+int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_nbr(va_list ap);
+int print_binary(va_list ap);
+int print_octal(va_list ap);
+int print_hexa_lower(va_list ap);
+int print_hexa_upper(va_list ap);
+int print_unsigned(va_list ap);
+int print_str_unprintable(va_list ap);
+int print_str_reverse(va_list ap);
+int print_ptr(va_list ap);
+int print_rot13(va_list ap);
+int print_percent(va_list ap __attribute__((unused)));
+int print_number(int n);
+int print_unsigned_number(unsigned int n);
+int _putchar(char c);
+int _puts(char *str, int ascii);
+int _nbr_len(int prmNumber);
+int _strlen_recursion(char *s);
+int convert_alpha_numeric(int nb, int upper);
+char *convert_rot13(char *str);
+char *convert_base(unsigned long nb, unsigned int base, int upper);
+char *_strdup(char *str);
+char *convert_base_pointer(unsigned long p);
+
 /**
- * struct char_funcs - Struct to find function based on char
- * @c: char to check as key
- * @f: Pointer to function to call
+ * struct flags_printf - struct conversion to function
+ * @c: flag string
+ * @f: pointer to func
  */
-typedef struct char_funcs
+
+typedef struct flags_printf
 {
 	char *c;
-	int (*f)();
-} char_funcs_t;
-
-void write_buffer(char buffer[], int *buflen, int *bufpos);
-void initialize_buffer(char buffer[]);
-int _printf(const char *format, ...);
-int print_c(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_s(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_number(int n, char buffer[], int *buflen, int *bufpos);
-int print_int(va_list args, char buffer[], int *buflen, int *bufpos);
-char *binConverter(unsigned int n);
-char *octConverter(unsigned int n);
-char *hexConverter(char type, unsigned int n);
-char *size_tHex(char type, size_t n);
-int print_Unum(unsigned int n, char buffer[], int *buflen, int *bufpos);
-int print_u(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_o(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_hex(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_heX(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_b(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_S(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_r(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_R(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_p(va_list args, char buffer[], int *buflen, int *bufpos);
+	int (*f)(va_list);
+} flags_p;
 
 #endif  /* _MAIN_H */
