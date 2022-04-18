4dc4ed70824b686fe89c35f3168cf4ee23abf2fb
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-18 13:38:33 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,34 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
 
+int _putchar(char c);
+int print_int(va_list arg);
+int print_unsigned(va_list arg);
 int _printf(const char *format, ...);
-int _position(const char *s, int n);
-int _strlen(char *s);
-int _abs(int n);
-int _numlen(int n);
-char *_strcat(char *dest, char *src, int n);
-void *rev_string(char *s);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+void print_binary(unsigned int n, unsigned int* printed);
+int print_unsignedToBinary(va_list arg);
+int print_oct(va_list arg);
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_STR (va_list arg);
 
 /**
- *  * struct type - structure datatype
- *   * @op: type arguement
- *    * @f: assofiated functio
- *     */
-
-typedef struct type
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
+ */
+typedef struct identifierStruct
 {
-		char *op;
-			char *(*f)(va_list);
-} type_t;
-
-char *print_c(va_list list);
-char *print_s(va_list list);
-char *print_i(va_list list);
-char *print_bin(va_list list);
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
 
 #endif /* #ifndef _MAIN_H */
