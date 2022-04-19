2772f50204df4db0549ddc8397ff557cb04712b0
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-19 12:47:53 +0300

    Update _typeof.c

--- _typeof.c
+++ _typeof.c
@@ -1,30 +1,69 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
- * _typefor - struct of format specifiers.
- * @argu: arguments passed.
- * @argb: position.
- * Return: 0.
+ *function_manager - function manager
+ *@c: character to find
+ *@arg: va_list type
+ *Description: This function call other functions
+ * when the character is found
+ *Return: count of printed characters
  */
-int (*_typefor(const char *argu, int argb))(va_list)
+int function_manager(char c, va_list arg)
 {
-  s_type types[] = {
-    {"c", p_char},
-    {"s", p_string},
-    {"d", p_int},
-    {"i", p_int},
-    {"R", print_R},
-    {"b", p_binary},
-    {NULL, NULL},
-  };
-  int i;
+	int cont = 0;
 
-  for (i = 0; types[i].args != NULL; i++)
-    {
-      if (types[i].args[0] == argu[argb])
+	cont = _switch(c, arg);
+	if (c == 'p')
+		cont = print_ptr(arg);
+	return (cont);
+}
+/**
+ *_switch - switch
+ *@c: character to find
+ *@arg: va_list type
+ *Description: This function evaluate cases
+ *Return: count of printed characters
+ */
+int _switch(char c, va_list arg)
+{
+	int cont = 0;
+
+	switch (c)
 	{
-	  return (types[i].func);
+		case 'b':
+			cont += print_unsign(arg, 2);
+			break;
+		case 'c':
+			cont += print_character(arg);
+			break;
+		case 'd':
+		case 'i':
+			cont += print_sign(arg, 10);
+			break;
+		case 'o':
+			cont += print_unsign(arg, 8);
+			break;
+		case 'r':
+			cont += print_rev(arg);
+			break;
+		case 'R':
+			cont += print_rot13(arg);
+			break;
+		case 's':
+			cont += print_string(arg);
+			break;
+		case 'u':
+			cont += print_unsign(arg, 10);
+			break;
+		case 'x':
+			cont += print_base16_upper_lower(arg, "0123456789abcdef");
+			break;
+		case 'X':
+			cont += print_base16_upper_lower(arg, "0123456789ABCDEF");
+			break;
+		default:
+			cont = -1;
 	}
-    }
-  return (NULL);
+	return (cont);
 }
