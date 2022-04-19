34441b1a87383b386ff51bfee946cd7daa85e290
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-19 12:56:52 +0300

    Update _typeof.c

--- _typeof.c
+++ _typeof.c
@@ -1,69 +1,39 @@
 #include "main.h"
 
-
 /**
- *function_manager - function manager
- *@c: character to find
- *@arg: va_list type
- *Description: This function call other functions
- * when the character is found
- *Return: count of printed characters
+ * get_print - selects the right printing function
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function loops through the structs array
+ * func_arr[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
+ * Return: a pointer to the matching printing function
  */
-int function_manager(char c, va_list arg)
+int (*get_print(char s))(va_list, flags_t *)
 {
-	int cont = 0;
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int flags = 14;
 
-	cont = _switch(c, arg);
-	if (c == 'p')
-		cont = print_ptr(arg);
-	return (cont);
-}
-/**
- *_switch - switch
- *@c: character to find
- *@arg: va_list type
- *Description: This function evaluate cases
- *Return: count of printed characters
- */
-int _switch(char c, va_list arg)
-{
-	int cont = 0;
+	register int i;
 
-	switch (c)
-	{
-		case 'b':
-			cont += print_unsign(arg, 2);
-			break;
-		case 'c':
-			cont += print_character(arg);
-			break;
-		case 'd':
-		case 'i':
-			cont += print_sign(arg, 10);
-			break;
-		case 'o':
-			cont += print_unsign(arg, 8);
-			break;
-		case 'r':
-			cont += print_rev(arg);
-			break;
-		case 'R':
-			cont += print_rot13(arg);
-			break;
-		case 's':
-			cont += print_string(arg);
-			break;
-		case 'u':
-			cont += print_unsign(arg, 10);
-			break;
-		case 'x':
-			cont += print_base16_upper_lower(arg, "0123456789abcdef");
-			break;
-		case 'X':
-			cont += print_base16_upper_lower(arg, "0123456789ABCDEF");
-			break;
-		default:
-			cont = -1;
-	}
-	return (cont);
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
 }
