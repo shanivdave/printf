68c538e93a698994ae8df95d68ae420ac5d23634
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-19 12:58:37 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,53 +1,45 @@
-#include"main.h"
-#include<stdio.h>
-#include<stdarg.h>
-#include<unistd.h>
-#include<stdlib.h>
-
+#include "main.h"
 /**
- *_printf - printf
- *@format: const char pointer
- *Description: this functions implement some functions of printf
- *Return: num of characteres printed
+ * _printf - printf function
+ * @format: const char pointer
+ * Return: b_len
  */
-
 int _printf(const char *format, ...)
 {
-  va_list list;
-  unsigned int i = 0, j = 0;
-  va_start(list, format);
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-  if (!format || (format[0] == '%' && format[1] == '\0'))
-    return (-1);
-  for (i = 0; format != NULL && format[i] != '\0'; i++)
-    {
-      if (format[i] == '%')
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-	  if (format[i + 1] == '%')
-	    {
-	      _putchar('%');
-	      j++;
-	      i++;
-	    }
-	  else if (_typefor(format, i + 1) != NULL)
-	    {
-	      j += _typefor(format, i + 1)(list);
-	      i++;
-	    }
-	  else
-	    {
-	      _putchar(format[i]);
-	      j++;
-	    }
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-      else
-	{
-	  _putchar(format[i]);
-	  j++;
-	}
-    }
-  va_end(list);
-  return (j);
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
 
 
