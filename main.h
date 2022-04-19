9443e44d9fe0062bca5bac82b7fa46efd0f11802
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-19 22:04:59 +0300

    group work

--- main.h
+++ main.h
@@ -3,6 +3,18 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
+#include <assert.h>
+#include <stdbool.h>
+
+/* macros */
+#define FLUSH -1
+#define PF_INIT {false, false, false}
+#define NIL "(nil)"
+#define HEXA "0x"
+#define NULL_STRING "(null)"
+#define NUL '\0'
+#define KILOBYTE 1024
+#define ABS(x) (((x) <  0) ? -(x) : (x))
 
 /**
  * struct flags - struct containing flags to "turn on"
