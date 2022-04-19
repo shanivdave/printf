79fe4b72df69032d828c584f99e2417834f7a0b5
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-19 11:11:59 +0300

    Update print_functions0.c

--- print_functions0.c
+++ print_functions0.c
@@ -1,87 +1,92 @@
 #include "main.h"
-/**
- * _printchar - prints a character
- * @ap: The character to print, argument pointer
- *
- * Return: 1
- */
-int _printchar(va_list ap)
-{
-	_putchar(va_arg(ap, int));
-	return (1);
-}
-/**
- * _printstring - prints a string
- * @ap: The string to print, argument pointer
- *
- * Return: i
- */
-int _printstring(va_list ap)
-{
-	char *s = va_arg(ap, char *);
-	int i = 0;
 
-	if (s == NULL)
-	s = "(null)";
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	return (i);
-}
-/**
- * _printnum - function that prints an integer
- * @n: integer to be printed by function.
- */
-void _printnum(int n)
+int p_char(va_list list)
 {
-	unsigned int m = n;
-
-	if (m / 10 != 0)
-	{
-		_printnum(m / 10);
-	}
-	_putchar(m % 10 + '0');
-}
-/**
- * _getcount - function that counts the length of an integer.
- * @n: integer to be counted.
- * Return: lenght of integer.
- */
-int _getcount(int n)
-{
-	int counter = 0;
-	unsigned int m = n;
-
-	while (m > 0)
-	{
-		m = m / 10;
-		counter++;
-	}
-	return (counter);
+  /*p_char prints character in list at position in va_arg*/
+  _putchar(va_arg(list, int));
+  return (1);
 }
 
-/**
- * _printint - function that prints an integer with helper function.
- * @ap: The string to print, argument pointer
- *
- * Return: counter
- */
-int _printint(va_list ap)
-{
-	int counter = 0;
-	int n = va_arg(ap, int);
 
-	if (n < 0)
+int p_string(va_list list)
+{
+  char *a;
+  int b;
+
+  a = va_arg(list, char*);
+  if (a == NULL)
+    {
+      a = "(null)";
+    }
+  for (b = 0; a[b] != '\0'; b++)
+    {
+      _putchar(a[b]);
+    }
+  return (b);
+}
+
+int p_int(va_list list)
+{
+  int a, b = 0;
+  unsigned int x;
+
+  a = va_arg(list, int);
+    if (a < 0)
+      {
+	_putchar('_');
+	b = 1;
+	x = a * (-1);
+      }
+    else
+      {
+	x = a;
+      }
+  if (x > 9)
+    {
+      return (b + count_num(x));
+    }
+  _putchar(x + '\0');
+  return (1 + b);
+}
+
+int count_num(unsigned int n)
+{
+  int count = 0;
+  unsigned int x, z;
+  if (n != 0)
+    {
+      z = (n / 10);
+      x = (n % 10);
+      count += count_num(z);
+      count++;
+      _putchar(x + '\0');
+      return (count);
+    }
+  return (0);
+}
+
+int p_rot13(char *a)
+{
+  int count = 0;
+  int x = 0;
+  int s = 0;
+  char z[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  char b[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+  for (x = 0; a[x] != '\0'; x++)
+    {
+      for (s = 0; z[s] != '\0'; s++)
 	{
-		n = -(n);
-		_putchar('-');
-		counter++;
+	  if (a[x] == z[s])
+	    {
+	      count += _putchar(b[s]);
+	      break;
+	    }
 	}
-	if (n == 0)
-		counter++;
-	counter += _getcount(n);
-	_printnum(n);
-	return (counter);
+      if (s > 51)
+	{
+	  count += _putchar(a[x]);
+	}
+    }
+  return (count);
 }
