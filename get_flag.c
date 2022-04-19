dbf865bf752086e586aa63e76c2cb6aea0b6d8da
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-19 19:17:41 +0300

    group work

--- get_flag.c
+++ get_flag.c
@@ -11,19 +11,27 @@ int get_flag(char s, flags_t *f)
 int i = 0;
 switch (s)
 {
-case '+':
-f->plus = 1;
-i = 1;
-break;
-case ' ':
-f->space = 1;
-i = 1;
-break;
-case '#':
-f->hash = 1;
-i = 1;
-break;
-}
+	case '+':
+		(*f).plus = 1;
+		i = 1;
+		break;
+	case ' ':
+		(*f).space = 1;
+		i = 1;
+		break;
+	case '#':
+		(*f).hash = 1;
+		i = 1;
+		break;
+	case '-':
+		(*f).minus = 1;
+		i = 1;
+		break;
+	case '0':
+		(*f).zero = 1;
+		i = 1;
+		break;
+	}
 return (i);
 }
 
