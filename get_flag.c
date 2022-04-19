ba668f03600de41f245b7c850b893b4bff9b3daf
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-19 19:26:46 +0300

    Update get_flag.c

--- get_flag.c
+++ get_flag.c
@@ -6,33 +6,26 @@
  *
  * Return: 1 if a flag has been turned on, 0 otherwise
  */
-int get_flags(char c, han_s *f)
+int get_flag(char s, flags_t *f)
 {
 	int i = 0;
 
-	switch (c)
+	switch (s)
 	{
-	case '+':
-		(*f).plus = 1;
-		i = 1;
-		break;
-	case ' ':
-		(*f).space = 1;
-		i = 1;
-		break;
-	case '#':
-		(*f).hash = 1;
-		i = 1;
-		break;
-	case '-':
-		(*f).minus = 1;
-		i = 1;
-		break;
-	case '0':
-		(*f).zero = 1;
-		i = 1;
-		break;
+		case '+':
+			f->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			f->space = 1;
+			i = 1;
+			break;
+		case '#':
+			f->hash = 1;
+			i = 1;
+			break;
 	}
+
 	return (i);
 }
 
